1320e912317c90e774a5c3eac6719311259b4e41
Hesusu-ikie <jespaepae@alum.us.es>
2023-01-21 00:35:15 +0100

    Implemented save file

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here. 
+onready var Money = $Money
+
+onready var game_data = SaveFile.game_data
 
 func _ready():
 	MusicController.play_tank_music()
-	pass
+	var font = Money.get_font("font")
+	font.size = 130
+	if game_data.size() != 0:
+		Money.text = String(game_data.money)
 
 func _on_ShopButton_pressed():
 	if get_tree().change_scene("res://scenes/Shop.tscn") != OK:
@@ -18,3 +22,8 @@ func _on_InventoryButton_pressed():
 	if get_tree().change_scene("res://scenes/Inventory.tscn") != OK:
 		print ("An unexpected error occured when trying to switch to the Inventory scene")
 
+func _on_MoneyButton_pressed():
+	print(game_data)
+	game_data.money += 1
+	Money.text = String(game_data.money)
+	SaveFile.save_data()
